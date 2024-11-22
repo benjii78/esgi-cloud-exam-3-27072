65b4efba796f5d8ead01a370eeb65aa4a5adcdd0
benjii178 <ballaire1@myges.fr>
2024-11-22 09:07:44 +0100

    premier

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'XJDZFJIVMV39RBXHD3LK', // TODO
+    secretAccessKey: '1XYelShF5AmAIgsj8NRsCwYDccs6DiGXc8Ye8hQQ', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-27072', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
